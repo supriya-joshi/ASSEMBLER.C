6030665bb2c238e08663b5cb31071b009b4e6651
supriya-joshi <supriya-joshi@users.noreply.github.com>
2015-11-27 23:03:25 +0530

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -2,39 +2,39 @@
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
-void main()
+void pass_one()
 {
-FILE *f1,*f2,*f3,*f4;
+FILE *f1,*f2,*f3,*f4,*f5;
 int locctr=0X0,start_ad=0X0,l,op1,o,len;
-char m[20],la[20],op[20],otp[20];
+char m[20],la[20],op[20],otp[20],record[100];
 f1=fopen("ques.txt","r");//open ques.txt (input file) in read mode
 f2=fopen("ques_inter.txt","w");//open intermediate file in write mode
 f3=fopen("ques_symtab.txt","w");//open file (symbol table) in write mode
-fscanf(f1,"%s %s %x",la,m,&op1);//input label,mnemonic,and operand(which becomes the starting address)
+fscanf(f1,"%s %s %X",la,m,&op1);//input label,mnemonic,and operand(which becomes the starting address)
+printf("\nintermediate  file\n\n\n");
 if(strcmp(m,"START")==0)
 {
  start_ad=op1;
  locctr=start_ad;//assigning starting address to location counter
-
- fprintf(f2,"\t%s\t%s\t%x\n",la,m,op1);//printing in intermediate file
-
- printf("\t%s\t%s\t%x\n",la,m,op1);
+ fprintf(f2,"%X\t%s\t%s\t%X\n",locctr,la,m,op1);//printing in intermediate file
+ printf("%X\t%s\t%s\t%X\n",locctr,la,m,op1);
  }
  else
+ {
  locctr=0;
+ }
 fscanf(f1,"%s %s",la,m);
 while(!feof(f1))
 {
  fscanf(f1,"%s",op);
- fprintf(f2,"\n%x\t%s\t%s\t%s\n",locctr,la,m,op);
-
- printf("\n%x\t%s\t%s\t%s\n",locctr,la,m,op);
+ fprintf(f2,"\n%X\t%s\t%s\t%s\n",locctr,la,m,op);
+ printf("\n%X\t%s\t%s\t%s\n",locctr,la,m,op);
  if(strcmp(la,"-")!=0)//if label found
  {
- fprintf(f3,"\n%x\t%s\n",locctr,la);//printing in symbol table file(label and its address)
+ fprintf(f3,"\n%X\t%s\n",locctr,la);//printing in symbol table file(label and its address)
  }
  f4=fopen("quesoptab.txt","r");//open file in read mode containing operation table
- fscanf(f4,"%s %x",otp,&o);
+ fscanf(f4,"%s %X",otp,&o);
  while(!feof(f4))
  {
   if(strcmp(m,otp)==0)
@@ -42,7 +42,7 @@ while(!feof(f1))
     locctr=locctr+3;
     break;
   }
-  fscanf(f4,"%s %x",otp,&o);
+  fscanf(f4,"%s %X",otp,&o);
   }
   fclose(f4);//closing file containing operation table
   if(strcmp(m,"WORD")==0)
@@ -71,11 +71,13 @@ while(!feof(f1))
      }
     fscanf(f1,"%s%s",la,m);
     }
-    if(strcmp(m,"END")==0)
-    {
-    printf("Program length =\n%d",locctr-start_ad);//prints length of program
-    }
+    printf("-\t-\tEND\t%06x\n",start_ad);
+    //printf("Program length =\n%x",locctr-start_ad);//prints length of program
+
+    fprintf(f2,"-\t-\tEND\t%06x",start_ad);
     fclose(f1);// closing input file
     fclose(f3);//closing file containing symbol table
-    getch();
-    }
+    fclose(f2);
+   // getch();
+  }
+  
